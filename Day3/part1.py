39f52afaf6f9c10522f799b1418e4115dec4100a
David Masterson <625842+davidma@users.noreply.github.com>
2025-12-03 13:36:17 +0000

    Added comments to day3 part1

--- Day3/part1.py
+++ Day3/part1.py
@@ -6,10 +6,16 @@ result = 0
 for line in file:
     digits = list(map(int,line.strip()))
 
+    ## Find the largest digit in the list, excluding the last digit
     a = max(digits[0:-1])
+
+    ## record the position of that digit
     p = digits.index(a)
+
+    ## find the largest digit to the right of that position
     b = max(digits[p+1:])
 
+    # put the two digits together and add to result
     result += a*10 + b
 
 print ('Result is: ' + str(result))
